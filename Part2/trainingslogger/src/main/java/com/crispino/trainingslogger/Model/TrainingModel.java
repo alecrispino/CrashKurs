b7de6c18496480c4dbe00714c2af7a47c78b3c33
Alessandro Crispino <alessandro_crispino@gmx.at>
2026-07-20 21:54:36 +0200

    connect db & add entity annotation + generated id

--- Part2/trainingslogger/src/main/java/com/crispino/trainingslogger/Model/TrainingModel.java
+++ Part2/trainingslogger/src/main/java/com/crispino/trainingslogger/Model/TrainingModel.java
@@ -1,10 +1,30 @@
 package com.crispino.trainingslogger.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class TrainingModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int dauerInMinuten;
     private boolean sparring;
+
+    protected TrainingModel(){}
+
+    public TrainingModel(String name, int dauerInMinuten, boolean sparring){
+        if (dauerInMinuten <= 0) {
+            throw new IllegalArgumentException("Dauer muss eine positive Zahl sein");
+        }
+        
+        this.name = name;
+        this.dauerInMinuten = dauerInMinuten;
+        this.sparring = sparring;
+    }
 
     public void setId(int id){
         this.id = id;
@@ -24,16 +44,6 @@ public class TrainingModel {
 
     public boolean isSparring(){
         return this.sparring;
-    }
-
-    public TrainingModel(String name, int dauerInMinuten, boolean sparring){
-        if (dauerInMinuten <= 0) {
-            throw new IllegalArgumentException("Dauer muss eine positive Zahl sein");
-        }
-        
-        this.name = name;
-        this.dauerInMinuten = dauerInMinuten;
-        this.sparring = sparring;
     }
 
     @Override
